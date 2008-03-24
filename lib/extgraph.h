bfaab93bd839ed3b9f55aab223e3041493ffe17e
Lionel Debroux <lionel_debroux@yahoo.fr>
2008-03-24 14:27:42 +0000

    Add ClipSpriteX8_MASK_R.
    git-svn-id: http://opensvn.csie.org/ExtGraph/trunk@28 d1c08d8a-23fe-0310-ada3-8a898e872ab9

--- lib/extgraph.h
+++ lib/extgraph.h
@@ -729,6 +729,7 @@ void ClipSprite32_XOR_R(short x asm("%d0"), short y asm("%d1"), short h asm("%d2
 
 
 void ClipSpriteX8_AND_R(short x asm("%d0"), short y asm("%d1"), short h asm("%d3"), short bytewidth asm("%d2"), unsigned char *sprt asm("%a1"), void *dest asm("%a0")) __attribute__((__regparm__));
+void ClipSpriteX8_MASK_R(short x asm("%d0"), short y asm("%d1"), short h asm("%d3"), short bytewidth asm("%d2"), unsigned char *sprt asm("%a1"), unsigned char *mask asm("%a2"), void *dest asm("%a0")) __attribute__((__regparm__));
 void ClipSpriteX8_OR_R(short x asm("%d0"), short y asm("%d1"), short h asm("%d3"), short bytewidth asm("%d2"), unsigned char *sprt asm("%a1"), void *dest asm("%a0")) __attribute__((__regparm__));
 void ClipSpriteX8_XOR_R(short x asm("%d0"), short y asm("%d1"), short h asm("%d3"), short bytewidth asm("%d2"), unsigned char *sprt asm("%a1"), void *dest asm("%a0")) __attribute__((__regparm__));
 
